b6d03809f3fe94286d1f7518c13a11c1bff3b5cb
xwuShirley <37637998+xwuShirley@users.noreply.github.com>
2024-08-08 18:15:39 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
-Result_Path=./results-405b-pulsar/
-TMP_NAME=eval_llama31_1
+Result_Path=/home/shirley/codes/evalplus/results-mbpp-8b/
+TMP_NAME=eval_llama31_2
+dataset=mbpp #humaneval
 mkdir -p ${Result_Path}/${TMP_NAME}
-export TOGETHER_API_KEY=??
-python codegen/generate.py --model "meta-llama/Meta-Llama-3.1-405B-Instruct" \
+export TOGETHER_API_KEY=485b6501ee67cc885f14bf500a77e9e0c696ce7c3967be1b44e1e6836061c7c2
+python codegen/generate.py --model "meta-llama/Meta-Llama-3.1-8B-Instruct" \
 --greedy --root ${Result_Path}/${TMP_NAME} \
---dataset humaneval --API pulsar #&> ${Result_Path}/${TMP_NAME}.log  #--backend vllm --tp 1
+--dataset mbpp --API together &> ${Result_Path}/${TMP_NAME}.log  #--backend vllm --tp 1
 
-# # Result_Path=/home/shirley/codes/evalplus/humaneval/eval_llama31_2/humaneval
-# Result_Path=/home/shirley/codes/evalplus/eval_llama31_2b/humaneval/
-# Name=meta-llama--Meta-Llama-3.1-8B-Instruct_vllm_temp_0.0 #deepseek-ai--deepseek-coder-6.7b-base_vllm_temp_0.0 
-# ls ${Result_Path}/${Name}
-# python codegen/merge.py ${Result_Path}/${Name} ${Result_Path}/${Name}.jsonl
-
-# cat ${Result_Path}/${Name}.jsonl
-# echo ${Result_Path}/${Name}.jsonl
-# # # #/home/shirley/codes/evalplus/humaneval/meta-llama--Meta-Llama-3.1-8B_vllm_temp_0.0.jsonl
-# python evalplus/sanitize.py --samples ${Result_Path}/${Name}.jsonl
+root=${Result_Path}/${TMP_NAME}/${dataset}
 
 
-# python evalplus/evaluate.py --dataset humaneval --samples ${Result_Path}/${Name}.jsonl
+Name=meta-llama--Meta-Llama-3.1-8B-Instruct_vllm_temp_0.0 #deepseek-ai--deepseek-coder-6.7b-base_vllm_temp_0.0 
+ls ${root}/${Name}
+python codegen/merge.py ${root}/${Name} ${root}/${Name}.jsonl &>> ${Result_Path}/${TMP_NAME}_eval.log
+
+cat ${root}/${Name}.jsonl
+echo ${root}/${Name}.jsonl
+python evalplus/sanitize.py --samples ${root}/${Name}.jsonl &>> ${Result_Path}/${TMP_NAME}_eval.log
+
+
+python evalplus/evaluate.py --dataset ${dataset} --samples ${root}/${Name}.jsonl &>> ${Result_Path}/${TMP_NAME}_eval.log
